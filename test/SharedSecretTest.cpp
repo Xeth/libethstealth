b0e2f97639c1d735b4cbe5139db94d73d17b32f2
Victor Bucataru <ping@devktor.com>
2016-01-26 12:08:25 +0200

    shared secret unit tests

---
+++ test/SharedSecretTest.cpp
@@ -0,0 +1,34 @@
+
+#include <boost/test/unit_test.hpp>
+
+#include "SharedSecretFactory.hpp"
+#include "ethkey/KeyGenerator.hpp"
+
+using Ethereum::KeyGenerator;
+
+BOOST_AUTO_TEST_SUITE(SharedSecretTest)
+
+
+
+BOOST_AUTO_TEST_CASE(generate)
+{
+
+    Ethereum::KeyGenerator generator;
+    Ethereum::KeyPair scan = generator.generate();
+    Ethereum::KeyPair ephemeral = generator.generate();
+
+    Ethereum::Stealth::SharedSecretFactory secretFactory;
+    Ethereum::Stealth::SharedSecret secret = secretFactory.createFromScanPublic(scan.getPublicKey(), ephemeral.getPrivateKey());
+    Ethereum::Stealth::SharedSecret secret2 = secretFactory.createFromScanSecret(scan.getPrivateKey(), ephemeral.getPublicKey());
+
+//    BOOST_REQUIRE_EQUAL(secret, secret2);
+    for(int i=0; i<secret.size(); i++)
+    {
+        BOOST_REQUIRE_EQUAL(secret[i], secret2[i]);
+    }
+}
+
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
