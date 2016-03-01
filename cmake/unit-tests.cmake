9467962826bd5bb6c37583a24077a13fd5c1bba0
Victor Bucataru <ping@devktor.com>
2016-03-01 02:34:38 +0200

    optional gmp

--- cmake/unit-tests.cmake
+++ cmake/unit-tests.cmake
@@ -2,6 +2,7 @@ find_package(Boost COMPONENTS system filesystem unit_test_framework random REQUI
 find_package(CryptoPP REQUIRED)
 find_package(EthKey REQUIRED)
 find_package(JsonCPP REQUIRED)
+find_package(GMP)
 
 file(GLOB TEST_SOURCES 
     "test/*.cpp"
@@ -13,4 +14,8 @@ include_directories(${Boost_INCLUDE_DIRS} ${CRYPTOPP_INCLUDE_DIR} ${JSONCPP_INCL
 add_executable(ethstealth-tests ${TEST_SOURCES})
 add_dependencies(ethstealth-tests ethstealth)
 
-target_link_libraries(ethstealth-tests ethstealth ${ETHKEY_LIBRARY} ${Boost_FILESYSTEM_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${Boost_RANDOM_LIBRARY} ${CRYPTOPP_LIBRARY} ${JSONCPP_LIBRARY} gmp)
+target_link_libraries(ethstealth-tests ethstealth ${ETHKEY_LIBRARY} ${Boost_FILESYSTEM_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${Boost_RANDOM_LIBRARY} ${CRYPTOPP_LIBRARY} ${JSONCPP_LIBRARY})
+
+if(GMP_LIBRARY)
+    target_link_libraries(ethstealth-tests ${GMP_LIBRARY})
+endif()
