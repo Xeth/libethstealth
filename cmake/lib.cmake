a02de55072a16c2ba7dcc2987e58451418537fb7
Victor Bucataru <ping@devktor.com>
2016-04-17 14:16:31 +0300

    optional target install

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -18,10 +18,10 @@ file(GLOB SOURCES
 
 
 
-
 add_library(ethstealth STATIC ${SOURCES})
 
-
-install(FILES ${HEADERS} DESTINATION include/ethstealth)
-install(FILES config.hpp DESTINATION include/ethstealth)
-install (TARGETS ethstealth ARCHIVE DESTINATION lib LIBRARY DESTINATION lib RUNTIME DESTINATION bin)
+if(NOT SKIP_LIBRARY_INSTALL)
+    install(FILES ${HEADERS} DESTINATION include/ethstealth OPTIONAL)
+    install(FILES config.hpp DESTINATION include/ethstealth OPTIONAL)
+    install(TARGETS ethstealth ARCHIVE DESTINATION lib LIBRARY DESTINATION lib RUNTIME DESTINATION bin OPTIONAL)
+endif()
