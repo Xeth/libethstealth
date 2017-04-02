8e70e68f3ea4a659abd3432b41ef4559a97340e0
Victor Bucataru <ping@devktor.com>
2017-04-02 23:23:42 +0300

    optional ethstealth-test target; using target_include_directories

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -7,18 +7,17 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/CopyHeaders.cmake)
 
 CopyHeaders()
 
-#add_definitions(-D__DEBUG__)
-
-include_directories(${Boost_INCLUDE_DIRS} ${CRYPTOPP_INCLUDE_DIR} ${JSONCPP_INCLUDE_DIR} ${ETHCRYPTO_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/src)
-
+if(${ENABLE_DEBUG})
+    add_definitions(-D__DEBUG__)
+endif()
 
 file(GLOB SOURCES 
     "src/*.cpp"
 )
 
-
-
 add_library(ethstealth STATIC ${SOURCES})
+
+target_include_directories(ethstealth PUBLIC ${Boost_INCLUDE_DIRS} ${CRYPTOPP_INCLUDE_DIR} ${JSONCPP_INCLUDE_DIR} ${ETHCRYPTO_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/src)
 
 if(NOT SKIP_LIBRARY_INSTALL)
     install(FILES ${HEADERS} DESTINATION include/ethstealth OPTIONAL)
--- cmake/unit-tests.cmake
+++ cmake/unit-tests.cmake
@@ -11,14 +11,12 @@ file(GLOB TEST_SOURCES
     "test/*.cpp"
 )
 
-
-include_directories(${Boost_INCLUDE_DIRS} ${CRYPTOPP_INCLUDE_DIR} ${JSONCPP_INCLUDE_DIR} ${ETHCRYPTO_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/src)
-
-add_executable(ethstealth-tests ${TEST_SOURCES})
+add_executable(ethstealth-tests EXCLUDE_FROM_ALL ${TEST_SOURCES})
 add_dependencies(ethstealth-tests ethstealth)
-
+target_include_directories(ethstealth-tests PUBLIC ${Boost_INCLUDE_DIRS} ${CRYPTOPP_INCLUDE_DIR} ${JSONCPP_INCLUDE_DIR} ${ETHCRYPTO_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/src)
 target_link_libraries(ethstealth-tests ethstealth ${ETHCRYPTO_LIBRARY} ${Boost_FILESYSTEM_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${Boost_RANDOM_LIBRARY} ${CRYPTOPP_LIBRARY} ${JSONCPP_LIBRARY})
 
 if(GMP_LIBRARIES)
     target_link_libraries(ethstealth-tests ${GMP_LIBRARIES})
 endif()
+
