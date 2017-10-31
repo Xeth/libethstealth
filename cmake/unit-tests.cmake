7b039191a79877b478e5d567ac88af7acabd32a2
Victor Bucataru <ping@devktor.com>
2017-10-31 15:59:32 +0200

    exporting library path

--- cmake/unit-tests.cmake
+++ cmake/unit-tests.cmake
@@ -4,8 +4,8 @@ file(GLOB TEST_SOURCES
 
 add_executable(ethstealth-tests EXCLUDE_FROM_ALL ${TEST_SOURCES})
 add_dependencies(ethstealth-tests ethstealth)
-target_include_directories(ethstealth-tests PUBLIC ${Boost_INCLUDE_DIRS} ${JSONCPP_INCLUDE_DIR} ${ETHCRYPTO_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/src)
-target_link_libraries(ethstealth-tests ethstealth ${ETHCRYPTO_LIBRARY} ${Boost_FILESYSTEM_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${Boost_RANDOM_LIBRARY} ${JSONCPP_LIBRARY})
+target_include_directories(ethstealth-tests PUBLIC ${Boost_INCLUDE_DIRS} ${JSONCPP_INCLUDE_DIR} ${ETHCRYPTO_INCLUDE_DIRS} ${CMAKE_CURRENT_SOURCE_DIR}/src)
+target_link_libraries(ethstealth-tests ethstealth ${ETHCRYPTO_LIBRARIES} ${Boost_FILESYSTEM_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${Boost_RANDOM_LIBRARY} ${JSONCPP_LIBRARY})
 
 if(GMP_LIBRARIES)
     target_link_libraries(ethstealth-tests ${GMP_LIBRARIES})
